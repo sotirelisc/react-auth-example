26434e1470c8a5dd6811b78fc5863bc405b27cf2
sotirelisc <gtakriz@hotmail.com>
2019-06-02 23:43:33 +0300

    Create & setup basic client views.

---
+++ client/src/components/auth/SignUp.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class SignUp extends React.Component {
+  render() {
+    return (
+      <form>
+        <fieldset>
+          <label>Email</label>
+
+        </fieldset>
+        <fieldset>
+          <label>Password</label>
+          
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+export default SignUp;
